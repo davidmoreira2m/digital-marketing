ec416e80763bb3b12da40ff11a22d2fe8193fe89
David Moreira <davidmoreiramaia@gmail.com>
2022-09-02 12:54:01 -0300

    Área Trabalhos Recentes

--- main.js
+++ main.js
@@ -8,4 +8,12 @@ window.onload = function digital_fn() {
       collapse.classList.toggle("active");
       // console.log(toggle);
    });
+
+   //masonry js
+   let grid = document.querySelector("#site-main .recent-work-area .images-flex");
+   let msnry = new Masonry(grid, {
+      itemSelector: ".flex-item",
+      gutter: 100,
+      fitWidth: true,
+   });
 };
